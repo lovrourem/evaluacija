2860599375115611ed2a334f08aaa8956e039a15
lovrourem <lovro.urem@fer.hr>
2025-09-21 23:19:57 +0200

    Logika za dohvacanje i uređivanje zadataka

--- VESTIGO EVALUACIJA/backend/src/main/java/evaluacijski_zadatak/services/TaskService.java
+++ VESTIGO EVALUACIJA/backend/src/main/java/evaluacijski_zadatak/services/TaskService.java
@@ -1,7 +1,12 @@
 package evaluacijski_zadatak.services;
 
+import java.util.List;
+
 import evaluacijski_zadatak.dtos.TaskDto;
 
 public interface TaskService {
     TaskDto createTask(TaskDto taskDto);
+    TaskDto getTaskById(Long taskId);
+    List<TaskDto> getAllTasks();
+    TaskDto updateTask(Long taskId, TaskDto updatedTask);
 }
