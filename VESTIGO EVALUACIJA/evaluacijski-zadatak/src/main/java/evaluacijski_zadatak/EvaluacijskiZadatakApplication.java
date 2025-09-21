5a4c99571c1b5f7564dde4cb8447571785c09ecf
lovrourem <lovro.urem@fer.hr>
2025-09-21 17:33:24 +0200

    Inicijalizacija Spring Boot

---
+++ VESTIGO EVALUACIJA/evaluacijski-zadatak/src/main/java/evaluacijski_zadatak/EvaluacijskiZadatakApplication.java
@@ -0,0 +1,13 @@
+package evaluacijski_zadatak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacijskiZadatakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacijskiZadatakApplication.class, args);
+	}
+
+}
---
+++ VESTIGO EVALUACIJA/evaluacijski-zadatak/src/test/java/evaluacijski_zadatak/EvaluacijskiZadatakApplicationTests.java
@@ -0,0 +1,13 @@
+package evaluacijski_zadatak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacijskiZadatakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
